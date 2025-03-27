affa08420b1a717761994884c59a3f22005bf860
xxxiu <129813663+morettt@users.noreply.github.com>
2025-03-27 20:59:31 +0800

    Update api_go.py

--- Mnemosyne-bert/api_go.py
+++ Mnemosyne-bert/api_go.py
@@ -8,7 +8,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 # 加载模型和tokenizer
-model_path = r"Mnemosyne-bert\output_models"
+model_path = r"Mnemosyne-bert"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
