3529508496e9867fea18275360e158a6f105267f
xxxiu <129813663+morettt@users.noreply.github.com>
2025-05-31 16:47:33 +0800

    Update Game-starts.bat

--- Game-starts.bat
+++ Game-starts.bat
@@ -5,7 +5,7 @@ cd %~dp0
 :: 启动第一个API
 start cmd /k "call conda activate my-neuro && python asr_api.py"
 :: 启动第二个API
-start cmd /k "call conda activate my-neuro &&cd tts-studio &&python tts_api.py -p 5000 -d cuda -s tts-model/FKTTS/fake_neuro.pth -dr tts-model/FKTTS/sama.wav -dt "Hold on please, I'm busy. Okay, I think I heard him say he wants me to stream Hollow Knight on Tuesday and Thursday." -dl "en""
+start cmd /k "call conda activate my-neuro &&cd tts-studio &&python tts_api.py -p 5000 -d cuda -s tts-model/merge.pth -dr tts-model/neuro/01.wav -dt "Hold on please, I'm busy. Okay, I think I heard him say he wants me to stream Hollow Knight on Tuesday and Thursday." -dl "en"
 :: 启动第三个API (BERT)
 start cmd /k "call conda activate my-neuro && python bert_api.py"
 :: 启动第四个API (Mnemosyne-bert)
