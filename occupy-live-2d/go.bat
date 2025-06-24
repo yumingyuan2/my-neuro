24ea62e370b2a0d43ae24c20380e5dd4feca1dfa
xxxiu <129813663+morettt@users.noreply.github.com>
2025-06-24 22:04:42 +0800

    Delete occupy-live-2d directory

--- occupy-live-2d/go.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-cd /d %~dp0
-echo 正在启动桌宠应用...
-.\node\node.exe .\node_modules\electron\cli.js .
-pause
--- occupy-live-2d/server-tools/start_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-echo 正在启动MCP服务器...
-"..\node\node.exe" server.js
-if %ERRORLEVEL% NEQ 0 pause
-exit
