0943179d4fc8256926fd7b6b3f6edc67ba2f2b83
jdnoeg <101007607+jdnoeg@users.noreply.github.com>
2025-08-03 01:30:07 +0800

    Add files via upload

--- fine_tuning/text/chinese2.py
+++ fine_tuning/text/chinese2.py
@@ -32,8 +32,8 @@ if is_g2pw:
 
     parent_directory = os.path.dirname(current_file_path)
     g2pw = G2PWPinyin(
-        model_dir="GPT_SoVITS/text/G2PWModel",
-        model_source=os.environ.get("bert_path", "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"),
+        model_dir=os.path.join(current_file_path, "G2PWModel"),
+        model_source=os.environ.get("bert_path", os.path.join(os.path.dirname(parent_directory), "tts-studio/pretrained_models/chinese-roberta-wwm-ext-large")),
         v_to_u=False,
         neutral_tone_with_five=True,
     )
