fadb0bc7a959fcd1127beea9469fc7e1151e2b8a
xxxiu <129813663+morettt@users.noreply.github.com>
2025-08-17 06:17:55 +0800

    Update ASR.bat

--- ASR.bat
+++ ASR.bat
@@ -2,5 +2,5 @@
 title 启动所有API服务
 :: 使用脚本所在目录作为工作目录
 cd %~dp0
-start cmd /k "call conda activate my-neuro && python run_server.py -a '0'"
+start cmd /k "call conda activate my-neuro && python asr_api.py"
 echo 所有API服务已启动!
