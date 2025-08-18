3df1bc468ba1db147d26a3d0f0ea932c5aac6d05
xxxiu <129813663+morettt@users.noreply.github.com>
2025-08-18 16:00:35 +0800

    Update TTS.bat

--- TTS.bat
+++ TTS.bat
@@ -1,6 +1,4 @@
 @echo off
-title 启动所有API服务
 :: 使用脚本所在目录作为工作目录
 cd %~dp0
-start cmd /k "call conda activate my-neuro && python run_server.py -a '1'"
-echo 所有API服务已启动!
+call conda activate my-neuro &&cd tts-studio &&python move_nltk.py &&python tts_api.py -p 5000 -d cuda -s tts-model/merge.pth -dr tts-model/neuro/01.wav -dt "Hold on please, I'm busy. Okay, I think I heard him say he wants me to stream Hollow Knight on Tuesday and Thursday." -dl "en"
