a8b2d95f964e004cf8cef435316d050ee859ddae
xxxiu <129813663+morettt@users.noreply.github.com>
2025-08-18 16:00:01 +0800

    Update bert.bat

--- bert.bat
+++ bert.bat
@@ -1,6 +1,4 @@
 @echo off
-title 启动所有API服务
 :: 使用脚本所在目录作为工作目录
 cd /d %~dp0
-start cmd /k "call conda activate my-neuro & python run_server.py -a '2'"
-echo 所有API服务已启动!
+call conda activate my-neuro && python omni_bert_api.py
