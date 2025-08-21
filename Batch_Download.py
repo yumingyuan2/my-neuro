b7b1efbdb1757aea3279b649d32f687a1272f6ae
xxxiu <129813663+morettt@users.noreply.github.com>
2025-08-21 17:50:47 +0800

    Update Batch_Download.py

--- Batch_Download.py
+++ Batch_Download.py
@@ -136,7 +136,7 @@ def download_live2d_model():
     """下载并解压Live 2D模型到live-2d文件夹"""
     print("\n========== 下载Live 2D模型 ==========")
 
-    url = "https://github.com/morettt/my-neuro/releases/download/v4.7.5/live-2d.zip"
+    url = "https://github.com/morettt/my-neuro/releases/download/v4.7.7/live-2d.zip"
     file_name = url.split('/')[-1]
     target_folder = "live-2d"
 
@@ -472,4 +472,5 @@ else:
     print("nltk_data下载成功！")
 
 print("\n所有下载操作全部完成！")
+
 
